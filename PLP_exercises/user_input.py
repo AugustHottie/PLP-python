d87454c71464bb5a576f07cad6e7fdb9047e9de0
augusthottie <chimexjessica@outlook.com>
2024-04-24 20:46:47 +0200

    first commit

---
+++ PLP_exercises/user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("Where are you located? ")
+
+message = f"Hello {name}, you are {age}, years old and live in {location}."
+print(message)
